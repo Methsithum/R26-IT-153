9d98fe8eb62e8e860607d6a732fadb8a6b34509a
Methsithum <rmethsithum@gmail.com>
2026-04-20 02:03:45 +0530

    Merge pull request #1 from Methsithum/Develop
    mistake

---
+++ backend/app/config/database.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+from app.config.settings import settings
+
+client = MongoClient(settings.mongodb_url)
+db = client[settings.database_name]
+
+
+def test_db_connection() -> bool:
+    try:
+        client.admin.command("ping")
+        return True
+    except Exception:
+        return False
