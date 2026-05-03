e93eb873c769a5a2445abb4e09bb0e87d2b6a04d
DamikaDeshan <damikadeshan40@gmail.com>
2026-05-03 16:04:58 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from app.routes.health import router as health_router
+from app.routes.user import router as user_router
+from app.routes.daily import router as daily_router
+from app.routes.reflection import router as reflection_router
 
 app = FastAPI(title="Smart Uni Guide API")
 
 app.include_router(health_router)
-
+app.include_router(user_router)
+app.include_router(daily_router)
+app.include_router(reflection_router)
 
 @app.get("/")
 def root():
