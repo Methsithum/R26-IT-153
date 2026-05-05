bfeeee327a197185878767dd57b175be14ce3426
Rusiru Nawodya <rusirunawo2001@gmail.com>
2026-05-05 23:46:34 +0530

    Update settings.py

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -1,10 +1,14 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+
+ENV_FILE = Path(__file__).resolve().parents[2] / ".env"
 
 class Settings(BaseSettings):
     mongodb_url: str
     database_name: str
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(env_file=ENV_FILE, extra="ignore")
 
 
 settings = Settings()
