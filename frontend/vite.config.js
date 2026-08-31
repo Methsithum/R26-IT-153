be02d9203c0c24e6e65d288be478a9eb7325f956
Rusiru Nawodya <rusirunawo2001@gmail.com>
2026-08-31 11:38:19 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
-    port: 5173,
+    port: 5174,
     strictPort: true,
     proxy: {
       "/api": {
